e1875ed013f9ff9bacd84fc61b481d8ddef2e697
he_guitang <he_guitang@163.com>
2018-08-14 18:13:00 +0800

    多线程:线程工具

--- src/multiThreading/demo/syncTool/SemaphoreTest.java
+++ src/multiThreading/demo/syncTool/SemaphoreTest.java
@@ -4,6 +4,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
+/**
+ *
+ */
 public class SemaphoreTest {
     public static void main(String[] args) {
         ExecutorService service = Executors.newCachedThreadPool();
