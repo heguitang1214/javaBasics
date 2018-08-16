e8c0e58c4bc825a2f5bee6d1c4749f5dea496913
he_guitang <he_guitang@163.com>
2018-08-16 16:40:22 +0800

    JSON:ObjectMap  JVM:直接内存

---
+++ src/json/entity/User.java
@@ -0,0 +1,12 @@
+package json.entity;
+
+/**
+ * @author he_guitang
+ * @version [1.0 , 2018/8/16]
+ * 用户实体
+ */
+public class User {
+
+
+
+}
---
+++ src/json/test/ObjectMapperTest.java
@@ -0,0 +1,22 @@
+package json.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author he_guitang
+ * @version [1.0 , 2018/8/16]
+ * ObjectMapper的使用测试
+ */
+public class ObjectMapperTest {
+
+    public static void main(String[] args) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        
+
+    }
+
+
+
+}
