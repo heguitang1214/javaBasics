4d40ec4ac8b2203d767cfebab07348cb2bcdfd65
he_guitang@163.com <hgt1391214>
2018-08-26 13:56:04 +0800

    Spring:AOP

---
+++ src/spring/aop/test/ProxyTest.java
@@ -0,0 +1,42 @@
+package spring.aop.test;
+
+import spring.aop.interceptor.LogInterceptor;
+import spring.ioc.dao.UserDAO;
+import spring.ioc.dao.impl.UserDAOImpl;
+import spring.ioc.model.User;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by 11256 on 2018/8/26.
+ * AOP动态代理测试
+ */
+public class ProxyTest {
+
+
+    public static void main(String[] args) {
+        testProxy();
+    }
+
+
+    private static void testProxy() {
+        UserDAO userDAO = new UserDAOImpl();
+        LogInterceptor li = new LogInterceptor();
+        li.setTarget(userDAO);
+        /*
+            产生的代理对象根据参数中的接口产生,生成的代理对象实现所需要的接口
+            目标的实现交给了代理对象来实现(添加增强的逻辑)
+         */
+        UserDAO userDAOProxy = (UserDAO) Proxy.newProxyInstance(
+                userDAO.getClass().getClassLoader(),
+//                userDAO.getClass().getInterfaces(),
+                new Class[]{UserDAO.class},
+                li);
+        System.out.println(userDAOProxy.getClass());
+        userDAOProxy.save(new User());
+
+    }
+
+
+
+}
