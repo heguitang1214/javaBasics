8b89b961125ff3fdddea0ca1d00f793ede1c6034
he_guitang <he_guitang@163.com>
2018-08-30 18:26:11 +0800

    CGLib factory

---
+++ src/designPatterns/proxy/cglib/factory/DaoFactory.java
@@ -0,0 +1,26 @@
+package designPatterns.proxy.cglib.factory;
+
+import net.sf.cglib.proxy.Enhancer;
+
+/**
+ * @author he_guitang
+ * @version [1.0 , 2018/8/30]
+ */
+class DaoFactory {
+
+    static Dao create() {
+
+        Enhancer enhancer = new Enhancer();
+
+        enhancer.setSuperclass(DaoImpl.class);
+
+        enhancer.setCallback(new Proxy());
+
+        Dao dao = (Dao) enhancer.create();
+
+        System.out.println("类型:" + dao.getClass());
+
+        return dao;
+    }
+
+}
