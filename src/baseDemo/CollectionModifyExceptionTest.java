4a1929903bfda0aae10a076f026671115e5df70a
he_guitang@163.com <hgt1391214>
2018-09-08 21:34:48 +0800

    集合迭代

--- src/baseDemo/CollectionModifyExceptionTest.java
+++ src/baseDemo/CollectionModifyExceptionTest.java
@@ -1,28 +1,32 @@
-package multiThreading.demo.syncTool;
+package baseDemo;
 
+import multiThreading.demo.syncTool.User;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  *	同步集合
+ *
+ *
  */
 public class CollectionModifyExceptionTest {
 
 	public static void main(String[] args) {
-//		Collection<User> users = new ArrayList<>();
-		Collection<User> users = new CopyOnWriteArrayList<>();
+		Collection<User> users = new ArrayList<>();
+//		Collection<User> users = new CopyOnWriteArrayList<>();
 		/*
 		 * ArrayList可能会造成无限循环
 		 */
-		users.add(new User("张三",28));
-		users.add(new User("李四",25));			
-		users.add(new User("王五",31));	
+		users.add(new User("张三1",28));
+		users.add(new User("李四1",25));
+		users.add(new User("王五1",31));
 		Iterator itrUsers = users.iterator();
 		while(itrUsers.hasNext()){
-//			System.out.println("aaaa");
+//			System.out.println("aaaaa");
 			User user = (User)itrUsers.next();
-			if("张三".equals(user.getName())){
+			if("李四1".equals(user.getName())){
 				users.remove(user);
 				//itrUsers.remove();
 			} else {
