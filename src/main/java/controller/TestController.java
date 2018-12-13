22104ee1ef5bb5a12169532317ee73fe0477e85d
ex-heguitang <ex-heguitang@banketech.com>
2018-12-13 22:42:57 +0800

    springBoot

---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String test(){
+        return "hello";
+    }
+
+
+
+}
