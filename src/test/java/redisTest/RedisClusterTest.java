b478dc5eb8d390ff9e423a9b817d948c23ba56fc
ex-heguitang <ex-heguitang@banketech.com>
2018-12-13 16:38:03 +0800

    redis集群

---
+++ src/test/java/redisTest/RedisClusterTest.java
@@ -0,0 +1,30 @@
+package redisTest;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class RedisClusterTest {
+    @Test
+    public void test() throws IOException {
+        Set<HostAndPort> jedisClusterNodes = new HashSet<>();
+        jedisClusterNodes.add(new HostAndPort("47.93.194.11", 6379));
+        jedisClusterNodes.add(new HostAndPort("47.93.194.11", 6380));
+        jedisClusterNodes.add(new HostAndPort("47.93.194.11", 6381));
+        jedisClusterNodes.add(new HostAndPort("47.93.194.11", 6382));
+        jedisClusterNodes.add(new HostAndPort("47.93.194.11", 6383));
+        jedisClusterNodes.add(new HostAndPort("47.93.194.11", 6384));
+        JedisCluster cluster = new JedisCluster(jedisClusterNodes);
+        //执行JedisCluster对象中的方法，方法和redis一一对应。
+        cluster.set("cluster-test", "my jedis cluster test");
+        String result = cluster.get("cluster-test");
+        System.out.println(result);
+        //程序结束时需要关闭JedisCluster对象
+        cluster.close();
+    }
+}
