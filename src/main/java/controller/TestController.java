65288ca88f086b30e01c5844ca05926e1c4efea1
ex-heguitang <ex-heguitang@banketech.com>
2018-12-14 14:17:55 +0800

    logback日志

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 public class TestController {
 
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String test(){
+        logger.info("进入请求 hello");
         return "hello11122223333";
     }
 
